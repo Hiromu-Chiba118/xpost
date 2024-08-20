40bcbc74727d718829306d964df923fdbdf8493e
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2024-08-20 15:30:50 +0900

    企画1

--- js/script.js
+++ js/script.js
@@ -48,5 +48,19 @@ jQuery(function ($) {
     $('html').toggleClass('is-fixed');
   });
 
-  
+  // アニメーション
+  // gsap.fromTo('.gallery__items .gallery__item',
+  //   { opacity: 0,y:10,  },
+  //   { opacity: 1,y:0, duration: 1 ,ease: "power3.out", stagger:0.2});
+
+  // ローディング
+  let tl=gsap.timeline();
+  tl.fromTo(".loader__title span",{  opacity: 0,  x:-10,},{  opacity: 1,  x:0,  stagger:0.08,});
+  tl.fromTo(".loader__title",{  opacity: 1,  y:0,},{  opacity: 0,  y:-15,});
+  tl.fromTo(".loader__item",{  clipPath:"inset(0% 0% 0% 0% )",},{  clipPath:"inset(0% 0% 100% 0% )",  duration:1,  stagger:0.2,  ease: "power3.inOut"  },"-=0.1");
+  tl.to(".loader",{  display:"none",});
+  tl.fromTo(".fv__title span",{ opacity: 0,yPercent:100,},{  opacity: 1, yPercent:0, ease:Power3.out,},"-=0.9");
+  tl.fromTo(".fv__text",{  opacity: 0,},{  opacity: 1,});
+
+
 });
