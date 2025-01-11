058cdfe1f688fef66e08d393ccbe9753ac11da81
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2025-01-11 17:33:22 +0900

    なびげーしょん配置無理矢理

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-  const swiper = new Swiper('.swiper', {
-    spaceBetween: 10,
-    slidesPerView: "auto",
-  });
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.button__next',
+    prevEl: '.button__prev',
+  },
+  spaceBetween: 40,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: true,
+  speed:300,
+});
+
+
