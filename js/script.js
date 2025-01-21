6e64235f7dd30a7fafc886b2632a993bd8aa74d4
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2025-01-21 16:51:47 +0900

    openning

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 
 const swiper = new Swiper('.swiper', {
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
   spaceBetween: 20,
   slidesPerView: "auto",
   speed:300,
