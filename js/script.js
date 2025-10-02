c3ad409e63044075887e87d2941107a295f016cb
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2025-10-02 20:47:43 +0900

    保存

--- js/script.js
+++ js/script.js
@@ -1,32 +1,67 @@
-// GSAPアニメーション
-const op = gsap.timeline();
-op.to('.loader__ship', { xPercent: 200, duration: 1.2, ease: 'power3.inOut' }, '+=0.3').to('.loader__logo', { opacity: 1, ease: 'power3.inOut', duration: 0.5 }, '-=0.4').to('.loader__ship', { opacity: 0, ease: 'power3.inOut', duration: 0.5 }, '<').to('.loader__logo', { x: 10, opacity: 0, ease: 'power3.inOut', duration: 0.6 }, '+=0.4').to('.loader', { opacity: 0, duration: 0.5 }).to('.loader', { display: 'none' }).fromTo('.mv__texts', { opacity: 0, filter: 'blur(10px)' }, { opacity: 1, filter: 'blur(0px)' }, '<').fromTo('.header, .mv__pickup , .fixed-button', { autoAlpha: 0 }, { autoAlpha: 1 }, '+=0.2');
-
-// なんとなく要素を変数に入れる
-const newsButton2 = document.querySelector('.news__button2');
-
-// スマホ時に処理しないようにレスポンシブ対応
-let mm = gsap.matchMedia();
-mm.add('(min-width: 768px)', () => {
-  // ホバーした時
-  newsButton2.addEventListener('mouseenter', function () {
-    // 初期値に戻す
-    gsap.set(this, { '--x': '-100%' });
-    // 動かす
-    gsap.to(this, {
-      '--x': '0%',
-      duration: 0.3,
-      ease: 'power3.inOut',
-    });
-  });
-
-  // ホバー外した時
-  newsButton2.addEventListener('mouseleave', function () {
-    // 動かす
-    gsap.to(this, {
-      '--x': '100%',
-      duration: 0.3,
-      ease: 'power3.inOut',
-    });
-  });
+jQuery(function ($) {
+  // 決まり文句
+  gsap.fromTo(
+    // 動かす対象
+    '.news__title',
+    {
+      x: -300,
+    },
+    {
+      x: 300,
+      duration: 2,
+      scrollTrigger: {
+        trigger: '.news__title',
+        start: 'top 80%',
+        scrub: true,
+      },
+    }
+  );
 });
+
+gsap.fromTo(
+  '.gallery__item',
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: '.gallery__item',
+      start: 'top 80%',
+    },
+  }
+);
+
+gsap.fromTo(
+  '.text span',
+  {
+    yPercent: 100,
+  },
+  {
+    yPercent: 0,
+    duration: 2,
+    stagger: 0.1,
+    ease: 'power4.inOut',
+    scrollTrigger: {
+      trigger: '.text',
+      start: 'top 80%',
+    },
+  }
+);
+
+gsap.fromTo(
+  '.test-img',
+  {
+    clipPath: 'inset(20% 20% 20% 20%)',
+  },
+  {
+    clipPath: 'inset(0% 0% 0% 0%)',
+    scrollTrigger: {
+      trigger: '.test-img',
+      start: 'top 80%',
+      end: 'bottom center',
+      scrub: 2,
+    },
+  }
+);
